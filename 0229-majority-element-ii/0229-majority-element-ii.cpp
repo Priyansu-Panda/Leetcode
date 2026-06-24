d2060b7016c06d0eab178aef425b46b83634594a
Priyansu Kumar Panda <151668436+Priyansu-Panda@users.noreply.github.com>
2026-06-25 00:59:00 +0530

    Time: 0 ms (100%), Space: 23.6 MB (62.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        map<int, int> freq;
         vector<int> ls;
-        int min = n/3 + 1;
-        for(int i=0; i<n; i++){
-            freq[nums[i]]++;
-            if(freq[nums[i]] == min){
-                ls.push_back(nums[i]);
+        int n = nums.size(), min = n/3 + 1;
+        int cnt1 = 0, el1 = 0;
+        int cnt2 = 0, el2 = 0;
+        for(int i = 0; i<n; i++){
+
+            if(cnt1 == 0 && nums[i]!=el2)        el1 = nums[i];
+            else if(cnt2 == 0 && nums[i]!=el1)   el2 = nums[i];
+            
+            if(el1 == nums[i])         cnt1++;
+            else if(el2 == nums[i])    cnt2++;
+            else{
+                cnt1--; cnt2--;
             }
-            if(ls.size() == 2)  break;
         }
-        sort(ls.begin(),ls.end());
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i = 0; i<n; i++){
+            if(el1 == nums[i])          cnt1++;
+            else if(el2 == nums[i])    cnt2++;
+        }
+        if(cnt1>=min)    ls.push_back(el1);
+        if(cnt2>=min)    ls.push_back(el2);
         return ls;
     }
 };
